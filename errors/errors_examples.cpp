60e13ab5f6dfdfe486366aaec5dcd93b75149a69
CalenDario13 <dariorusso.dr13@gmail.com>
2022-12-19 17:37:26 +0100

    COMMIT[2] - Minor Fixes

---
+++ errors/errors_examples.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+/*
+int main(){
+    // Simulate a Compile Time Error
+    std::cout << "Hello World!" << std::endl
+    return 0;
+}
+*/
+
+/*
+int main(){
+    // Simulate a Run Time Error
+    int value = 7 / 0;
+    std::cout << value << std:endl;
+    return 0;
+}
+*/
