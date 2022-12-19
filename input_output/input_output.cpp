eaef05244e13f6e04934ed657380558ee153df53
CalenDario13 <dariorusso.dr13@gmail.com>
2022-12-19 18:24:43 +0100

    COMMIT[9] - Minor FIxes

--- input_output/input_output.cpp
+++ input_output/input_output.cpp
@@ -9,15 +9,21 @@ int main(){
     int age;
     std::string surname;
     std::string name;
+    std::string full_name;
 
+    /*
     std::cout << "Enter your name and surname separted by a space: " << std::endl;
     std::cin >> name >> surname;
+    */
 
+    std::cout << "Type your full name: " << std::endl;
+    std::getline(std::cin, full_name);
     std::cout << "Enter your age: " << std::endl;
     std::cin >> age;
 
     // Print a Message
-    std::cout << "You are " << name << " " << surname << " and you are " << age << " years old." << std::endl;
+    // std::cout << "You are " << name << " " << surname << " and you are " << age << " years old." << std::endl;
+    std::cout << "You are " << full_name << " and you are " << age << " years old." << std::endl;
     // Print a Log
     std::clog << "This is a Log message" << std::endl;
     // Print an Error
