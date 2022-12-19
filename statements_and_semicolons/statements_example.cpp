6885dbf4d7a5bb2cc42c69d630fd8517bf8a1705
CalenDario13 <dariorusso.dr13@gmail.com>
2022-12-19 17:54:25 +0100

    COMMIT[3] - Statements and Functions

---
+++ statements_and_semicolons/statements_example.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int addNumbers(int first_number, int second_number){
+    int sum = first_number + second_number;
+    return sum;
+}
+
+int main(int argc, char **argv){
+    
+    int firstNumber = 12;
+    int secondNumber = 9;
+
+    int sum = addNumbers(firstNumber, secondNumber);
+
+    std::cout << "The sum of the two is: " << sum << std::endl;
+
+    return 0;
+}
