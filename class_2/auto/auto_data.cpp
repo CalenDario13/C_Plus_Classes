f09a7f212e7fa8e56f540aedcc8d2da98a022362
CalenDario13 <dariorusso.dr13@gmail.com>
2022-12-20 22:55:19 +0100

    COMMIT[22] - Auto cast and Value Assignment Lecture

--- class_2/auto/auto_data.cpp
+++ class_2/auto/auto_data.cpp
@@ -11,6 +11,21 @@ int main(){
     auto var5 {123u}; //unsigned integer
     auto var6 {123ul}; //usnigned long integer
 
+    // ASSIGN A NEW VALUE
+    int initial_value {};
+    std::cout << "Give me a positive integer number" << std::endl;
+    std::cin >> initial_value;
+    initial_value = 33;
+    std::cout << "I have changed your number in " << initial_value << std::endl;
+
+    // PAY ATTENTION TO ASSIGN VALUES TO AUTO CATSED VARIABLES
+    auto var7{333u};
+    std::cout << "----------------------------" << std::endl;
+    std::cout << "The original Values is " << var7 << std::endl;
+    var7 = -22;
+    std::cout << "But after assigning  a negative value (-22) to variable which was casted as u int, the compiler assign a junk value to the variable. " 
+        << var7 << std::endl;
+
     return 0;
 
 }
