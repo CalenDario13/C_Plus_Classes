bcfe36c8390a8063c6e592725a36ebd625060e44
CalenDario13 <dariorusso.dr13@gmail.com>
2023-01-10 16:07:53 +0100

    COMMIT[53] - pointers_memory_leaks

---
+++ class_7_pointers/memory_leaks/memory_leaks.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int main(){
+    
+    /*
+
+    // In This situation After assigning number to p_num, p_num points to address2, 
+    //  But Address1 still exists, but we cannot access it
+    // It is a WASTE OF MEMORY, DON'T DO IT.
+
+    int* p_num { new int{67} };
+    int number{55};
+
+    p_number = &number
+
+    // OR
+
+    {
+    int* p_num2 {new int{55}};     
+    // You shopuld delete and reset pointer here   
+    }
+    // exiting from the brackets pointer does not exists anymore, but the address yes.
+
+    // OR
+
+    int* p_num3 { new int{44} };
+
+    // we should delete and reset the pointer here
+    // delete p_num3;
+    // p_num3 = nullptr;
+
+    p_num3 = new int{44};
+
+    */
+
+    return 0;
+
+}
