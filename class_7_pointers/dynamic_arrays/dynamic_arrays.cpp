638df777578b982cb325d06532f0485a33c67098
CalenDario13 <dariorusso.dr13@gmail.com>
2023-01-10 16:20:36 +0100

    COMMIT[54] - array_dynamic_allocation

---
+++ class_7_pointers/dynamic_arrays/dynamic_arrays.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main(){
+    
+    const size_t size{10};
+    double* p_salaries { new double[size]{3,5.5,8.7,3.9,4.6} } ;
+
+
+    if(p_salaries){
+        std::cout << "Size of the array pointer is: " << sizeof(p_salaries) << std::endl;
+        std::cout << "----------------" << std::endl;
+        for(size_t i = 0; i < size; ++i){
+            std::cout << "value: " << p_salaries[i] << " or " << *(p_salaries + i) << std::endl;
+        }
+
+    }
+
+    delete[] p_salaries;
+    p_salaries = nullptr;
+
+
+    return 0;
+
+}
